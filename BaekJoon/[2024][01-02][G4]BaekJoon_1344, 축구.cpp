cc8ef57b515eacefc468637ee9cabf436f8292c2
RealTone <realtone98@gmail.com>
2024-03-04 12:41:31 +0900

    rename 01-02

--- BaekJoon/[2024][01-02][G4]BaekJoon_1344, 축구.cpp
+++ BaekJoon/[2024][01-02][G4]BaekJoon_1344, 축구.cpp
@@ -1,35 +1,41 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 float A, B; // A와 B팀이 골을 넣을 확률
 float answer = 0;
-int sosu[] = { 2,3,5,7,11,13,17 };
+int sosu[] = {2, 3, 5, 7, 11, 13, 17};
 float dp[19][19][19]; // 총 라운드는 18개 골수도 18골
 // A만 소수, B만 소수, 둘다 소수 or 둘다 소수 X
 
-float play_game(int round, int a, int b) {
+float play_game(int round, int a, int b)
+{
 
-	if (round == 18) {
-		for (int x : sosu) {
-			if (a == x || b == x) {
+	if (round == 18)
+	{
+		for (int x : sosu)
+		{
+			if (a == x || b == x)
+			{
 				return 1;
 			}
 		}
 		return 0;
 	}
-	if (dp[round][a][b]) return dp[round][a][b];
+	if (dp[round][a][b])
+		return dp[round][a][b];
 
 	dp[round][a][b] += play_game(round + 1, a + 1, b + 1) * A * B;
 	dp[round][a][b] += play_game(round + 1, a + 1, b) * A * (1 - B);
-	dp[round][a][b] += play_game(round + 1, a, b + 1) * (1-A) * B;
+	dp[round][a][b] += play_game(round + 1, a, b + 1) * (1 - A) * B;
 	dp[round][a][b] += play_game(round + 1, a, b) * (1 - A) * (1 - B);
 
 	return dp[round][a][b];
 }
 
-int main() {
+int main()
+{
 
 	cin >> A >> B;
 	A /= 100;
